a5fe9f7e61900927d9faf5b5dbceba07e2d2ba6c
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2019-09-14 12:11:18 +0200

    feat: sample with dynamic component loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component';
+import { TemplateDirective } from './template.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'split-dynamic';
+export class AppComponent implements OnInit {
+  @ViewChild(TemplateDirective, { static: true }) template: TemplateDirective;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  ngOnInit() {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(DialogComponent);
+    const viewContainerRef = this.template.viewContainerRef;
+    viewContainerRef.clear();
+    const componentRef = viewContainerRef.createComponent(componentFactory);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { TemplateDirective } from './template.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    TemplateDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    DialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
